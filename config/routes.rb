8fd58c5448d540013702f311d4c59e5faff36568
shaswat-satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-08-30 09:11:19 +0530

    Updated Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index'
-  root 'home#index'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
   # Defines the root path route ("/")
